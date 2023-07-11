9a9aca1a7b68d1261abdc246aa6b9af0a0494ba6
dpstrip <dpstrip@gmail.com>
2023-07-11 21:45:55 +0000

    Added an EC2 server

--- lib/my_vpc-stack.ts
+++ lib/my_vpc-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import {readFileSync} from 'fs';
 
 export class MyVpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -42,6 +44,11 @@ export class MyVpcStack extends cdk.Stack {
         ec2.Port.tcp(80),
         'allow HTTP access from anywhere');
         
+      webSG.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(443),
+        'allow HTTPS access from anywhere');  
+        
          /*Create a security groups and there rules*/
       const privateSG = new ec2.SecurityGroup(this, 'davidsmyvpc-private',{
         vpc,
@@ -59,6 +66,35 @@ export class MyVpcStack extends cdk.Stack {
         ec2.Port.allIcmp()
         );
         
-        
-  }
+      /* Create a IAM role and a server to put into the public instance */
+      
+      //Create role
+      const publicserverRole = new iam.Role(this, 'publicserver-role',
+      {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+        managedPolicies:[
+          iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
+          ],
+      });
+      
+      //Add a EC2 instance
+      const ec2PublicInstance = new ec2.Instance(this, 'my-ec2-instance', {
+        vpc,
+        vpcSubnets:{
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        role: publicserverRole,
+        securityGroup: webSG,
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.BURSTABLE2,
+          ec2.InstanceSize.MICRO
+          ),
+          machineImage: new ec2.AmazonLinuxImage({
+            generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,})
+          });
+          
+          //load in a web server to test with
+          const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
+          ec2PublicInstance.addUserData(userDataScript);
+      }
 }
